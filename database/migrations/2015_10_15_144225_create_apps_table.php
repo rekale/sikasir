e9c25150e895032932703067dadd5f9d5e3340a9
vagrant <vagrant@homestead>
2015-10-15 14:58:11 +0000

    create app class model and tables

---
+++ database/migrations/2015_10_15_144225_create_apps_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apps', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('member_id')->unsigned()->index();
+            $table->string('username');
+            $table->string('password');
+            
+            $table->foreign('member_id')
+                  ->references('id')
+                  ->on('members')
+                  ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('apps');
+    }
+}
