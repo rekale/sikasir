76e816e8b5e16a71f2306c92702f2488eddfb7e5
vagrant <vagrant@homestead>
2015-10-15 14:17:12 +0000

    create member class model and tables

---
+++ database/migrations/2015_10_15_140325_create_members_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('full_name');
+            $table->string('business_name');
+            $table->string('phone');
+            $table->text('address');
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('members');
+    }
+}
