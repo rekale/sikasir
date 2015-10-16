159af6a71623ceb5f1a24c6136f0e5d9fa74e05c
rekale <rekale1123@gmail.com>
2015-10-16 13:53:20 +0000

    create template for databaseseeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,6 +5,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
+     
+    protected $tables = [
+        'users',
+        'members',
+        'operator',
+        'apps',
+        'outlets',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $seeders = [
+        
+    ];
+
     /**
      * Run the database seeds.
      *
@@ -14,8 +30,37 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
+        $this->cleanMysqlDatabase();
+
+        foreach ($this->seeders as $seedClass)
+        {
+            $this->call($seedClass);
+        }
 
         Model::reguard();
+    }
+
+    private function cleanMysqlDatabase()
+    {
+        
+        $this->command->info('Truncating existing tables');
+        
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+
+        foreach ($this->tables as $table)
+        {
+            DB::table($table)->truncate();
+        }
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+
+    }
+
+    private function cleanPgsqlDatabase()
+    {
+    
+        $this->command->info('Truncating existing tables');
+        DB::statement('TRUNCATE TABLE ' . implode(',', $this->tables). '  RESTART IDENTITY;');
+    
     }
 }
