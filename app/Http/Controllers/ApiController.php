277def2ae651e9624c94dafd737eff06b6d8fa78
rekale <rekale1123@gmail.com>
2015-10-19 08:15:34 +0000

    add many respond function in apicontroller

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,12 +2,78 @@
 
 namespace Sikasir\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use League\Fractal\Manager;
+use League\Fractal\Resource\Item;
+use League\Fractal\Resource\Collection;
+use Illuminate\Http\Request;
 
-abstract class Controller extends BaseController
+class ApiController extends Controller
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    private $fractal;
+    private $statusCode = 200;
+
+    public function __construct(Manager $fractal, Request $request) 
+    {
+        $this->fractal = $fractal;
+        
+        if ( $request->input('include') ) {
+            $this->fractal->parseIncludes( $request->input('include') );
+        }
+    }
+  
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+    
+    public function setStatusCode($code)
+    {
+        $this->statusCode = $code;
+        
+        return $this;
+    }
+    
+    protected function respondWithItem($item, $callback)
+    {
+        $resource = new Item($item, $callback);
+        
+        $rootScope = $this->fractal->createData($resource);
+        
+        return $this->respondWithArray($rootScope->toArray()); 
+    }
+    
+    protected function respondWithCollection($collection, $callback)
+    {
+        $resource = new Collection($collection, $callback);
+        
+        $rootScope = $this->fractal->createData($resource);
+        
+        return $this->respondWithArray($rootScope->toArray()); 
+    }
+    
+    protected function respondWithArray(array $array, array $headers = [])
+    {
+        return response()->json($array, $this->statusCode, $headers);    
+    }
+    
+    protected function respondNotFound($msg = 'Not Found')
+    {
+        return $this->setStatusCode(404)->respondWithError($msg);
+    }
+    
+    protected function respondWithError($msg)
+    {
+        return $this->respond([
+            'error' => [
+                'message' => $msg,
+                'code' => $this->getStatusCode(),
+            ]
+        ]);
+    }
+    
+    protected function respond($data, $headers=[])
+    {
+        return response()->json($data, $this->getStatusCode(), $headers);
+    }
+    
 }
