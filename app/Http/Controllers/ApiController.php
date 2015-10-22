b1405bff029439bea575ce1cdb3c2a623c4cbc71
rekale <rekale1123@gmail.com>
2015-10-22 18:04:43 +0700

    make respondPaginated function to return paginated resource

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,6 +6,7 @@ use League\Fractal\Manager;
 use League\Fractal\Resource\Item;
 use League\Fractal\Resource\Collection;
 use Illuminate\Http\Request;
+use League\Fractal\Pagination\IlluminatePaginatorAdapter;
 
 class ApiController extends Controller
 {
@@ -61,18 +62,72 @@ class ApiController extends Controller
         return $this->respond($rootScope->toArray()); 
     }
     
+    /**
+     * 
+     * return data from collection to json
+     * 
+     * @param \Illuminate\Support\Collection $collection
+     * @param Closure $callback
+     */
     protected function respondWithCollection($collection, $callback)
     {
         $resource = new Collection($collection, $callback);
-        
+     
+        $resource->setPaginator(new IlluminatePaginatorAdapter($paginator));
+         
         $rootScope = $this->fractal->createData($resource);
         
         return $this->respond($rootScope->toArray()); 
     }
     
+    /**
+     * 
+     * return data from paginator to json
+     * 
+     * @param Paginator $paginator
+     * @param Closure $callback
+     */
+    protected function respondWithPaginated($paginator, $callback)
+    {
+    
+        $collection = $paginator->getCollection();
+        
+        $resource = new Collection($collection, $callback);
+        
+        $rootScope = $this->fractal->createData($resource);
+        
+        return $this->respond($rootScope->toArray()); 
+    
+    }
+    
+    /**
+     * resource not found
+     * 
+     * @param string $msg
+     */
     protected function respondNotFound($msg = 'Not Found')
     {
         return $this->setStatusCode(404)->respondWithError($msg);
+    }
+    
+    /**
+     * resource not found
+     * 
+     * @param string $msg
+     */
+    protected function respondCreated($msg = 'created')
+    {
+        return $this->setStatusCode(201)->respondSuccess($msg);
+    }
+    
+    protected function respondSuccess($msg)
+    {
+        return $this->respond([
+            'success' => [
+                'message' => $msg,
+                'code' => $this->getStatusCode(),
+            ]
+        ]);
     }
     
     protected function respondWithError($msg)
