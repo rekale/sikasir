efd2406a8f472146005c6b638ceef247a609eb9c
rekale <rekale1123@gmail.com>
2015-10-22 12:28:51 +0700

    change to local database

--- config/database.php
+++ config/database.php
@@ -1,11 +1,12 @@
 <?php
+/*
 $url = parse_url(getenv("DATABASE_URL"));
 
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
-
+*/
 return [
 
     /*
@@ -72,10 +73,10 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => $host,
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
+            'host'     => 'localhost',
+            'database' => 'sikasir',
+            'username' => 'postgres',
+            'password' => 'secret',
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
