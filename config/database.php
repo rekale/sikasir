55aca9efddb0dfcf64b75dd1a12b5e09ee1eba1b
rekale <rekale1123@gmail.com>
2015-10-23 06:43:41 +0700

    change config database for push to server

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
-/*
+
 $url = parse_url(getenv("DATABASE_URL"));
 
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
-*/
+
 return [
 
     /*
@@ -73,10 +73,10 @@ return [
 
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => 'localhost',
-            'database' => 'sikasir',
-            'username' => 'postgres',
-            'password' => 'secret',
+            'host'     => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
