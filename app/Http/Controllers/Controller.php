72de801f2ee6b8a3af22ff405e2be1b28313b6be
rekale <rekale1123@gmail.com>
2015-11-02 13:50:15 +0700

    delete api controller and move the apirepondable trait to controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,10 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Sikasir\Traits\IdObfuscater;
+use Sikasir\Traits\ApiRespondable;
 
 abstract class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests, ApiRespondable, IdObfuscater;
 }
--- app/Traits/ApiRespondable.php
+++ app/Traits/ApiRespondable.php
@@ -61,6 +61,16 @@ trait ApiRespondable
         return $this->setStatusCode(201)->respondSuccess($msg);
     }
     
+    /**
+     * resource failed to create
+     * 
+     * @param string $msg
+     */
+    protected function respondCreateFailed($msg = 'fail to create')
+    {
+        return $this->setStatusCode(409)->respondWithError($msg);
+    }
+    
     
     protected function respondSuccess($msg)
     {
@@ -86,5 +96,52 @@ trait ApiRespondable
     protected function respond($data, $headers=[])
     {
         return response()->json($data, $this->getStatusCode(), $headers);
+    }
+    
+    protected function respondWithItem($item, $callback)
+    {
+        $resource = new Item($item, $callback);
+        
+        $rootScope = $this->fractal->createData($resource);
+        
+        return $this->respond($rootScope->toArray()); 
+    }
+    
+    /**
+     * 
+     * return data from collection to json
+     * 
+     * @param \Illuminate\Support\Collection $collection
+     * @param Closure $callback
+     */
+    protected function respondWithCollection($collection, $callback)
+    {
+        $resource = new Collection($collection, $callback);
+     
+        $rootScope = $this->fractal->createData($resource);
+        
+        return $this->respond($rootScope->toArray()); 
+    }
+    
+    /**
+     * 
+     * return data from paginator to json
+     * 
+     * @param Paginator $paginator
+     * @param Closure $callback
+     */
+    protected function respondWithPaginated($paginator, $callback)
+    {
+    
+        $collection = $paginator->getCollection();
+        
+        $resource = new Collection($collection, $callback);
+        
+        $resource->setPaginator(new IlluminatePaginatorAdapter($paginator));
+        
+        $rootScope = $this->fractal->createData($resource);
+        
+        return $this->respond($rootScope->toArray()); 
+    
     }
 }
