7b7c1a2bf083111fe9c322086913716e633d60a2
rekale <rekale1123@gmail.com>
2015-11-22 09:23:56 +0700

    add gender to employeetransformmer

--- app/Transformer/EmployeeTransformer.php
+++ app/Transformer/EmployeeTransformer.php
@@ -12,20 +12,21 @@ use Sikasir\User\Employee;
 class EmployeeTransformer extends TransformerAbstract
 {
     use \Sikasir\Traits\IdObfuscater;
-    
+
     public function transform(Employee $employee)
     {
         return [
             'id' => $this->encode($employee->id),
             'name' => $employee->name,
+            'gender' => $employee->gender,
             'title' => $employee->title,
             'email' => $employee->user->email,
-            'address' => $employee->address, 
-            'phone'=> $employee->phone, 
-            'void_access' => (boolean) $employee->void_access, 
+            'address' => $employee->address,
+            'phone'=> $employee->phone,
+            'void_access' => (boolean) $employee->void_access,
             'icon' => $employee->icon
-            
+
         ];
     }
-    
+
 }
