f9cb06f85026e515dab8b8410432369be4587c32
rekale <rekale1123@gmail.com>
2015-12-10 11:36:15 +0700

    make idObfuscater abilities to encode and decode array value

--- app/V1/Traits/IdObfuscater.php
+++ app/V1/Traits/IdObfuscater.php
@@ -18,17 +18,51 @@ trait IdObfuscater
     
     private $salt = 'dDLZyl93F8TnO0GAw1riV5b4QoKsc2vx6tMeYmI7pNBXauWJqkjPCgSEfHhURz';
     
+    
+    /**
+     *  encode the id
+     * 
+     * @param type string|array $id
+     * @return string|array
+     */
     protected function encode($id)
     {
         $tiny = new \ZackKitzmiller\Tiny($this->salt);
         
+        if (is_array($id)) {
+            
+            $decodedId = [];
+            
+            foreach ($id as $name => $encodedId) {
+                $decodedId[$name] = $tiny->to($encodedId);
+            }
+            
+            return $decodedId;
+        }
+        
         return $tiny->to($id);
     }
     
-    
+    /**
+     * decode the encoded id
+     * 
+     * @param type string|array $id
+     * @return string|array
+     */
     protected function decode($id)
     {
         $tiny = new \ZackKitzmiller\Tiny($this->salt);
+        
+        if (is_array($id)) {
+            
+            $decodedId = [];
+            
+            foreach ($id as $name => $encodedId) {
+                $decodedId[$name] = $tiny->from($encodedId);
+            }
+            
+            return $decodedId;
+        }
         
         return $tiny->from($id);
     }
