f40bd4c1afbc84747c5445f48c59eec6d7c42451
rekale <rekale1123@gmail.com>
2016-02-06 15:58:01 +0700

    add nego field again in order_product

--- database/migrations/2016_01_06_124509_order_product_table.php
+++ database/migrations/2016_01_06_124509_order_product_table.php
@@ -15,7 +15,8 @@ class OrderProductTable extends Migration
         Schema::create('order_product', function (Blueprint $table) {
             $table->integer('order_id')->unsigned()->index();
             $table->integer('product_id')->unsigned()->index();
-            $table->integer('total')->default(0);
+            $table->unsignedInteger('total')->default(0);
+            $table->unsignedInteger('nego')->default(0);
             $table->timestamps();
             
             $table->foreign('order_id')
