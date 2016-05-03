05853999f803e53e30bed0a715a80f6bfa54ae94
rekale <rekale1123@gmail.com>
2016-05-03 22:11:29 +0700

    update doc format product

--- resources/views/doc/format.php
+++ resources/views/doc/format.php
@@ -145,20 +145,6 @@
                     "alert": true,
                     "alert_at": "10"
                 },
-                {//kalo mau hapus tambah field delete:true
-                    "delete": true,
-                    "id": "Z",
-                    "name": "kampret", 
-                    "barcode": "34234", 
-                    "price_init" : 10000,
-                    "price" : 234234,
-                    "icon" : "http://lorempixel.com/200/100/food",
-                    "countable": true, 
-                    "track_stock": true,
-                    "stock": 22,
-                    "alert": true,
-                    "alert_at": "10"
-                },
             ]
         }
 
