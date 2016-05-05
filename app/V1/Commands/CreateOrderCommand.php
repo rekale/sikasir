0920bced86b92c791fb791e6ea10f88dca1e9b98
rekale <rekale1123@gmail.com>
2016-05-05 22:14:47 +0700

    add another commit for previos=us commit

--- app/V1/Commands/CreateOrderCommand.php
+++ app/V1/Commands/CreateOrderCommand.php
@@ -10,10 +10,26 @@ use Sikasir\V1\Products\Variant;
 
 class CreateOrderCommand extends CreateCommand 
 {
+	private $auth;
+	
+	/**
+	 *
+	 * @param AuthInterface $auth
+	 *
+	 * @return $this
+	 */
+	public function setAuth(AuthInterface $auth)
+	{
+		$this->auth = $auth;
+	
+		return $this;
+	}
 	
 	public function execute() 
 	{
 		\DB::transaction(function () {
+			
+			$this->data['user_id'] = $this->auth->currentUser()->id;
 			
 			$order = $this->factory->create($this->data);
 			
@@ -22,13 +38,27 @@ class CreateOrderCommand extends CreateCommand
 				$order->variants()->attach(
 					$variant['id'],
 					[
-							'total' => $variant['quantity'],
-							'nego' => $variant['nego'],
+						'total' => $variant['quantity'],
+						'nego' => $variant['nego'],
 					]
 				);
 				
 			}
 			
+			//jika transaksi di hutangin
+			if( $this->orderIsDebt() ) {
+				
+			}
+			
 		});
+	}
+	
+	/**
+	 * 
+	 * @return boolean
+	 */
+	private function orderIsDebt()
+	{
+		return isset($this->data['due_date']) && isset($this->data['total']);
 	}
 }
