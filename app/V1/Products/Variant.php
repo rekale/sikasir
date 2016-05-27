a6cc1affce75351bbcb80aef1ff4e3fa17360850
rekale <rekale1123@gmail.com>
2016-05-27 20:00:54 +0700

    add current_weight in variants table

--- app/V1/Products/Variant.php
+++ app/V1/Products/Variant.php
@@ -9,7 +9,7 @@ use Sikasir\V1\Orders\Order;
 class Variant extends Model
 {
     protected $table = 'variants';
-    
+
       /**
      * The attributes that are mass assignable.
      *
@@ -17,36 +17,37 @@ class Variant extends Model
      */
     protected $fillable = [
         'product_id',
-        'name', 
-        'barcode', 
+        'name',
+        'barcode',
         'price_init',
         'price',
         'countable',
         'track_stock',
         'stock',
     	'current_stock',
+    	'current_weight',
         'alert',
         'alert_at',
         'icon',
         ];
-    
+
     public function product()
     {
         return $this->belongsTo(Product::class, 'product_id');
     }
-    
+
     public function outlets()
     {
         return $this->belongsToMany(Outlet::class)
                 ->withTimestamps()
                 ->withPivot(['total', 'nego']);
     }
-    
+
     public function orders()
     {
         return $this->belongsToMany(Order::class)
                     ->withPivot(['total', 'nego'])
                     ->withTimestamps();
     }
-    
+
 }
