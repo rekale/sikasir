77645a179533a00cf98b3fe35dc47503d94baa9e
rekale <rekale1123@gmail.com>
2016-05-31 13:51:34 +0700

    add calculation_type to show in product json

--- app/V1/Transformer/ProductTransformer.php
+++ app/V1/Transformer/ProductTransformer.php
@@ -16,7 +16,7 @@ use Sikasir\V1\Transformer\PurchaseOrderTransformer;
 class ProductTransformer extends TransformerAbstract
 {
    use \Sikasir\V1\Traits\IdObfuscater;
-   
+
     /**
      * List of resources possible to include
      *
@@ -30,16 +30,17 @@ class ProductTransformer extends TransformerAbstract
         'outs',
         'opnames',
         'purchases',
-    ]; 
-    
+    ];
+
     public function transform(Product $product)
     {
         $data = [
             'id' => $this->encode($product->id),
-            'name' => $product->name, 
-            'description' => $product->description, 
+            'name' => $product->name,
+            'description' => $product->description,
             'icon' => $product->icon,
             'unit' => $product->unit,
+            'calculation_type' => $product->calculation_type,
         ];
         //if it's the report
         if (isset($product->total))
@@ -47,59 +48,59 @@ class ProductTransformer extends TransformerAbstract
             $data['total'] = (int) $product->total;
             $data['amounts'] = (int) $product->amounts;
         }
-        
+
         return $data;
     }
-   
+
     public function includeVariants(Product $product)
     {
         $variants = $product->variants;
-        
+
         return $this->collection($variants, new VariantTransformer);
-        
+
     }
-    
+
     public function includeCategory(Product $product)
     {
         $item = $product->category;
-        
+
         return $this->item($item, new CategoryTransformer);
     }
-    
+
     public function includeOutlet(Product $variant)
-    {   
+    {
         $item = $variant->outlet;
-        
+
         return $this->item($item, new OutletTransformer);
-        
+
     }
-    
+
     public function includeEntries(Product $product)
     {
         $collection = $product->entries;
-        
+
         return $this->collection($collection, new InventoryTransformer);
     }
-    
+
     public function includeOuts(Product $product)
     {
         $collection = $product->outs;
-        
+
         return $this->collection($collection, new InventoryTransformer);
     }
-    
+
     public function includeOpnames(Product $product)
     {
         $collection = $product->opnames;
-        
+
         return $this->collection($collection, new OpnameTransformer);
     }
-    
+
     public function includePurchases(Product $product)
     {
         $collection = $product->purchases;
-        
+
         return $this->collection($collection, new PurchaseOrderTransformer);
     }
-    
+
 }
