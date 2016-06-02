083a2617da264b5769288ca4eea61231591ed61b
rekale <rekale1123@gmail.com>
2016-06-02 20:54:37 +0700

    when new outlet is created, insert product(for_all_outlets) to new the outlet

--- app/V1/Commands/CreateOutletCommand.php
+++ app/V1/Commands/CreateOutletCommand.php
@@ -6,36 +6,62 @@ use Sikasir\V1\Commands\CreateCommand;
 use Sikasir\V1\Interfaces\AuthInterface;
 use Sikasir\V1\User\User;
 
-class CreateOutletCommand extends CreateCommand 
+use Sikasir\V1\Products\Product;
+
+class CreateOutletCommand extends CreateCommand
 {
 	private $auth;
-	
+
 	/**
-	 * 
+	 *
 	 * @param AuthInterface $auth
-	 * 
+	 *
 	 * @return $this
 	 */
 	public function setAuth(AuthInterface $auth)
 	{
 		$this->auth = $auth;
-		
+
 		return $this;
 	}
-	
-	public function execute() 
+
+	public function execute()
 	{
 		\DB::transaction(function () {
-			
+
 
 			$outlet = $this->factory->create($this->data);
-				
+
+			//when new outlet is created, assign owner to every new outlet
 			$owner = User::whereTitle('owner')
 							->whereCompanyId($this->auth->getCompanyId())
 							->first();
-			
 			$owner->outlets()->attach($outlet->id);
-			
+
+			//insert products (for all outlets) to new outlet
+			$products = Product::with('variants')->distinct()
+								->whereForAllOutlets(true)
+								->get();
+
+			foreach ($products as $product) {
+				$product->outlet_id = $outlet->id;
+				$newProduct = Product::create($product->toArray());
+
+				foreach($product->variants as $variant) {
+
+					$variant->stock = 0;
+					$variant->current_stock = 0;
+					$variant->current_weight = 0;
+
+					$newProduct->variants()->create($variant->toArray());
+				}
+
+			}
+
+
+
+
+
 		});
 	}
 }
--- app/V1/Commands/CreateProductCommand.php
+++ app/V1/Commands/CreateProductCommand.php
@@ -8,49 +8,47 @@ use Sikasir\V1\Util\Obfuscater;
 use Sikasir\V1\Products\Product;
 use Sikasir\V1\Products\Variant;
 
-class CreateProductCommand extends CreateCommand 
+class CreateProductCommand extends CreateCommand
 {
 	private $auth;
-	
+
 	/**
-	 * 
+	 *
 	 * @param AuthInterface $auth
-	 * 
+	 *
 	 * @return $this
 	 */
 	public function setAuth(AuthInterface $auth)
 	{
 		$this->auth = $auth;
-		
+
 		return $this;
 	}
-	
-	public function execute() 
+
+	public function execute()
 	{
 		\DB::transaction(function () {
-			
-			
+
+
 			$this->data['outlet_ids'] = Obfuscater::decode($this->data['outlet_ids']);
 
 			$this->data['company_id'] = $this->auth->getCompanyId();
-			
+
 			foreach ($this->data['outlet_ids'] as $outletId) {
-				
+
 				//attach product to each outlets and save variants
-				
-				
 				$this->data['outlet_id'] = $outletId;
-					
+
 				$product = Product::create($this->data);
-				
+
 				//create variant Models
 				foreach($this->data['variants'] as $variant){
-				
+
 					$product->variants()->create($variant);
 				}
-				
+
 			}
-			
+
 		});
 	}
 }
