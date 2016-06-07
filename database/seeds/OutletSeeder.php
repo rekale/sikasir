f01a8d7609501bc0d49e7c53f4f8216b5cc168b1
rekale <rekale1123@gmail.com>
2016-06-07 11:30:18 +0700

    change kredit to kartu

--- database/seeds/OutletSeeder.php
+++ database/seeds/OutletSeeder.php
@@ -18,7 +18,7 @@ class OutletSeeder extends Seeder
     public function run()
     {
     	$businessField = [];
-    	
+
         $businessField[] = BusinessField::create([
             'name' => 'F&B',
         ])->id;
@@ -28,51 +28,51 @@ class OutletSeeder extends Seeder
         $businessField[] = BusinessField::create([
             'name' => 'Komoditas',
         ])->id;
-        
-        
+
+
         //create an outlet for every company
         Company::all()->each(function($company) use ($businessField){
-                
+
                 //create tax for each company
                 $taxes = factory(Tax::class, 3)->create([
                     'company_id' => $company->id,
                 ]);
-                
+
                 //create discount
                 factory(Discount::class, 3)->create([
                     'company_id' => $company->id,
                 ]);
-                
+
                 //create payment
                 factory(Payment::class)->create([
                     'company_id' => $company->id,
                     'name' => 'Tunai',
                 ]);
-                
+
                 //create payment
                 factory(Payment::class)->create([
                     'company_id' => $company->id,
-                    'name' => 'Kredit',
-                ]);                
+                    'name' => 'Kartu',
+                ]);
         });
-        
+
         $company = Company::findOrFail(1);
-        
+
         //create outlet for each company and add tax
         $outlets = factory(Outlet::class, 3)->create([
         		'company_id' => $company->id,
         		'tax_id' => $company->taxes->random()->id,
         		'business_field_id' => $businessField[mt_rand(0, 2)],
         ]);
-        
+
         //lists created employees id
-         
+
         $owner = $company->users()->where('title', '=', 'owner')->lists('id');
-         
+
         //add employees to every outlets
         foreach ($outlets as $outlet) {
         	$employees = $company->users()->where('title', '<>', 'owner')->get()->random(100)->lists('id');
-        	 
+
         	$outlet->users()->attach($employees->toArray());
         	$outlet->users()->attach($owner->toArray());
         }
