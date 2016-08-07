436a3db29a4263a404e3302b75984e64f9417585
rekale <rekale1123@gmail.com>
2016-08-07 10:54:46 +0700

    chnage query where company_id to specific tablename.company_id

--- app/V1/Repositories/EloquentCompany.php
+++ app/V1/Repositories/EloquentCompany.php
@@ -17,42 +17,42 @@ class EloquentCompany implements Interfaces\QueryCompanyInterface
      * @var Illuminate\Database\Eloquent\Model
      */
     private $model;
-    
+
     /**
      *
      * @var integer
      */
     private $companyId;
-    
+
     /**
-     * 
+     *
      * @param Model $model
      * @param integer $companyId
      */
-    public function __construct(Model $model, $companyId) 
+    public function __construct(Model $model, $companyId)
     {
         $this->model = $model;
         $this->companyId = $companyId;
     }
-    
+
     /**
-     * 
+     *
      * @return Builder
      */
     public function forCompany()
     {
-        return $this->model->whereCompanyId($this->companyId);
+        return $this->model->where($this->model->getTable().'.company_id', '=', $this->companyId);
     }
 
     /**
-     * 
+     *
      * @param array $data
      * @return Model
      */
-    public function dataForCompany(array $data) 
+    public function dataForCompany(array $data)
     {
         $data['company_id'] = $this->companyId;
-        
+
         return $this->model->fill($data);
     }
 
