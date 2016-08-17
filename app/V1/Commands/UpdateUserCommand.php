bf28bf71db9c3471577d8666ca02ee70272f2415
haikal <muhamad.haekal@gojek.com>
2016-08-17 17:17:06 +0700

    fix bug user title is change when update

--- app/V1/Commands/UpdateUserCommand.php
+++ app/V1/Commands/UpdateUserCommand.php
@@ -6,40 +6,37 @@ use Sikasir\V1\Commands\UpdateCommand;
 use Sikasir\V1\User\Authorizer;
 use Sikasir\V1\Util\Obfuscater;
 
-class UpdateUserCommand extends UpdateCommand 
+class UpdateUserCommand extends UpdateCommand
 {
-	
-	public function execute() 
+
+	public function execute()
 	{
 		\DB::transaction(function () {
-				
+
 			$user = $this->repo->find($this->id);
-			
+
 			$authorizer = new Authorizer($user);
-			
+
 			if ($this->data['title'] === 1) {
-				$this->data['title'] = 'staff';
 				$authorizer->managerDefault();
 			}
 			if ($this->data['title'] === 2) {
-				$this->data['title'] = 'kasir';
 				$authorizer->cashierDefault();
 			}
 			if ($this->data['title'] === 3) {
-				$this->data['title'] = 'owner';
 				$authorizer->ownerDefault();
 			}
-				
+
 			$this->data['password'] = bcrypt($this->data['password']);
-			
+
 			$user->update($this->data);
-			
+
 			$authorizer->syncAccess($this->data['privileges']);
-			
+
 			$outletIds = Obfuscater::decode($this->data['outlet_id']);
-			
+
 			$user->outlets()->sync($outletIds);
-			
+
 		});
 	}
 }
